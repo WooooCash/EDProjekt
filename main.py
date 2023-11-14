a022dca877b1ee679dc8cb21d2b3a2f9e9ed43a9
lbialczak <lbialczak@grupazpr.pl>
2023-11-14 17:20:07 +0100

    refactor gui - make window classes and move read_button into panel

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+# from app.image_frame import ImageFrame
+from src.panel import Panel
+
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        # self.image_frame = ImageFrame(self, width=700, height=700)
+        self.panel = Panel(self)
+
+        self.panel.grid(row=0, column=0)
+        # self.image_frame.grid(row=0, column=1)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("TK_Grafika")
+    app = MainApplication(root)
+    app.pack(side="top", fill="both", expand=True)
+
+    root.mainloop()
---
+++ src/panel.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import filedialog
+
+from src.data import Data
+
+
+class Panel(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        self.structure_label = tk.Label(self, width=20)
+        self.structure_label.grid()
+
+        # Narazie tu trzymamy data, ale docelowo nie powinno być w panelu, a w innej częsci
+        # w której będziemy faktycznie wyświetlać dane i wykonywać na nich operacje
+        self.data = Data()
+
+        bt_load = tk.Button(self, text="Otwórz plik", command=self.__read_data)
+        bt_debug_data = tk.Button(
+            self, text="Debug", command=lambda: print(self.data)
+        )
+
+        bt_load.grid(sticky=tk.EW)
+        bt_debug_data.grid(sticky=tk.EW)
+
+    def __read_data(self):
+        file = filedialog.askopenfile()
+
+        # parametry delimiter i has_headers trzeba brać z jakiś inputów "Entry"
+        self.data.read_file(file, delimiter=" ", has_headers=False)
