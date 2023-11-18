46726ec84b6361d7b841b0aecb8c982cbb6d56fa
lbialczak <lbialczak@grupazpr.pl>
2023-11-18 19:00:08 +0100

    refactor panel init

--- src/panel.py
+++ src/panel.py
@@ -1,8 +1,10 @@
+import codecs
 import tkinter as tk
 from tkinter import filedialog
-import codecs
 
 from src.data import Data
+
+SEPARATOR_MAPPING = {"Średnik": ";", "Tabulator": "\t", "Spacja": " "}
 
 
 class Panel(tk.Frame):
@@ -15,31 +17,45 @@ class Panel(tk.Frame):
         # Narazie tu trzymamy data, ale docelowo nie powinno być w panelu, a w innej częsci
         # w której będziemy faktycznie wyświetlać dane i wykonywać na nich operacje
         self.data = Data()
-        self.header = tk.BooleanVar()
-        self.separator_options = []
 
-        values = {"Średnik": ";",
-                  "Tabulator": "\t",
-                  "Spacja": " "}
-        self.separator = tk.StringVar(self, "1")
+        # VARS
+        header = tk.BooleanVar()
+        separator = tk.StringVar(self, " ")
 
-        for (text, value) in values.items():
-            self.separator_options.append(tk.Radiobutton(self, text=text, variable=self.separator, value=value, background="light blue"))
-
-        header_options = tk.Checkbutton(self, text='Nagłówek?', variable=self.header, onvalue=True, offvalue=False)
-
-        bt_load = tk.Button(self, text="Otwórz plik", command=self.__read_data)
-        bt_debug_data = tk.Button(
-            self, text="Debug", command=lambda: print(self.data)
+        # UI ELEMENTS
+        header_options = tk.Checkbutton(
+            self, text="Nagłówek?", variable=header, onvalue=True, offvalue=False
         )
+        separator_options = self.__generate_separator_options(separator)
+        bt_load = tk.Button(
+            self,
+            text="Otwórz plik",
+            command=lambda: self.__read_data(header.get(), separator.get()),
+        )
+        bt_debug_data = tk.Button(self, text="Debug", command=lambda: print(self.data))
+
+        # PLACEMENT
         header_options.grid(sticky=tk.EW)
-        for option in self.separator_options:
+        for option in separator_options:
             option.grid(sticky=tk.EW)
         bt_load.grid(sticky=tk.EW)
         bt_debug_data.grid(sticky=tk.EW)
 
-    def __read_data(self):
-        file = codecs.open(filedialog.askopenfilename(), encoding='utf-8')
+    def __read_data(self, has_headers: bool, separator: str):
+        file = codecs.open(filedialog.askopenfilename(), encoding="utf-8")
+        self.data.read_file(file, delimiter=separator, has_headers=has_headers)
 
-        # parametry delimiter i has_headers trzeba brać z jakiś inputów "Entry"
-        self.data.read_file(file, delimiter=str(self.separator), has_headers=bool(self.header))
+    def __generate_separator_options(self, variable: tk.StringVar):
+        options_list = []
+        for text, value in SEPARATOR_MAPPING.items():
+            options_list.append(
+                tk.Radiobutton(
+                    self,
+                    text=text,
+                    variable=variable,
+                    value=value,
+                    background="light blue",
+                )
+            )
+
+        return options_list
